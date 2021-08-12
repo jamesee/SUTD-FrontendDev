727b306da489cf3707fb09b9760da24229501709
jamesee <james.ee.developer@gmail.com>
2021-08-12 21:06:51 +0800

    added data/data.js

--- 2021-08-06/homework-day1/homework.md
+++ 2021-08-06/homework-day1/homework.md
@@ -6,6 +6,8 @@
 ## CareerForm component (80% done)
 reference: https://www.digitalocean.com/community/tutorials/how-to-build-forms-in-react
 
+see src/career.jsx
+
 ### Help needed.
 I have problem in binding the formData.summary (text-area). Somehow the handleChange function is not working on text-area changes.
 
--- 2021-08-06/homework-day1/src/pages/career.jsx
+++ 2021-08-06/homework-day1/src/pages/career.jsx
@@ -1,95 +1,14 @@
 import * as React from "react";
 import { CareerItem } from "../components/career-item";
 import { useState, useReducer } from "react";
-
-const jobs = [
-  {
-    descriptions: [
-      'Add comments on GitHub issue with "+1", "This is not Stack Overflow", and "Closed because irrelevant"',
-      "Push a commit everyday by removing/adding a space to make repository seems active.",
-      "Copy paste entire content of a random logs file and use it to raise GitHub issue on competitor's repository to create the perception of bugginess of the library.",
-    ],
-    requirements: [
-      "Master degree or outstanding writing skill is required.",
-      "Good sense of humour.",
-    ],
-    _id: "60f1bf2ddcd48a001f4f9e57",
-    title: "GitHub Issue Commentor",
-    department: "Engineering",
-    level: "internship",
-    summary:
-      "The Engineering teams at Shopit is the core competency of Shopit by utilizing next-gen Blockchain virtual currency and display them in an interactive platform using VR/AR best practices.",
-    headcount: 2,
-    createdAt: "2021-07-16T17:17:33.144Z",
-    updatedAt: "2021-07-16T17:17:33.144Z",
-    __v: 0,
-  },
-
-  {
-    descriptions: [
-      "Primarily handle inquiries pertaining to orders, payments, shipping, vouchers and etc from both sellers and buyers via live chat",
-      "Level 1 Customer Service is expected to provide First Contact Resolution (FCR) according to standard operating procedures. In the event of non-FCR, you are expected to follow up and respond back to users via callback or email",
-      "Update case details appropriately, i.e. Gdocx, Salesforce and etc",
-      "To meet Key Performance Indicators such as average handling time, FCR rate, email/chat response rate, customer satisfaction score and etc",
-      "Communicate effectively by providing accurate information to Shopee users and external partners such as Poslaju and iPay88, where appropriate",
-      "Communicate effectively with team members",
-      "Take on ad-hoc projects/assignments",
-    ],
-    requirements: [
-      "Passionate Diploma holder and fresh graduates are welcome to apply",
-      "Business writing skill is mandatory",
-      "Good typing skill",
-      "Ability to work independently, flexible and adaptable",
-      "Ability to multi-task, prioritize and manage time effectively",
-      "Demonstrate teamwork all the time",
-    ],
-    _id: "60f1bf2ddcd48a001f4f9e43",
-    title: "Customer Service Agent",
-    department: "Operations",
-    level: "entry",
-    summary:
-      "The Operation teams at Shopit covers the operational end-to-end process, from when the buyer searches for a product listed on the Shopit platform, to the moment the buyer receives the products. The team analyses and monitors operational KPIs across the region and conducts root cause analysis when operation performance fluctuates. The Operations team comprises Customer Service, Payment, Listings, Warehouse, Logistics, Seller Operations and Fraud.",
-    headcount: 10,
-    createdAt: "2021-07-16T17:17:33.112Z",
-    updatedAt: "2021-07-16T17:17:33.112Z",
-    __v: 0,
-  },
-  {
-    descriptions: [
-      "Explore and optimize the visual design work flow/method and help team improving the efficiency of cooperation",
-      "Act as a designer and one of the decision maker to define and design the key visual and guidance of each request from marketing and product, for better execution around the world",
-      "Optimize and maintain the brand identity for each Shopit brands by using scientific and efficient methods",
-    ],
-    requirements: [
-      "Bachelor degree or outstanding design skills with more than 2 years experience in marketing design / web design / mobile design",
-      "Solid design philosophy, deep understanding and practical experience in marketing/branding design, user experience design and related fields",
-      "Excellent communication and teamwork skills",
-      "Passionate and detail-oriented",
-      "Experience in design for e-commerce is a plus",
-      "Experience in designing for global/regional/local markets is a plus",
-      "Experience in project management is a plus",
-      "Experience in photography/graphic design/motion design is a plus",
-    ],
-    _id: "60f1bf2ddcd48a001f4f9e38",
-    title: "Web Designer",
-    department: "UI / UX Design",
-    level: "experienced",
-    summary:
-      "The Design team plays an important role in Shopit. The team covers the entire range of product UI/UX design, including the user-growth, promotion, wallet and payment, checkout, retention, listings and users, search and recommendation etc. The team is also responsible for the branding visual identities and elements of Shopit, including logos, mascots, stickers, and internal physical products.",
-    headcount: 5,
-    createdAt: "2021-07-16T17:17:33.086Z",
-    updatedAt: "2021-07-16T17:17:33.086Z",
-    __v: 0,
-  },
-];
-
+import { jobs } from "../data/data"
 
 const formReducer = (state, event) => {
   return {
     ...state,
     [event.name]: event.value
   }
- }
+}
 
 const CareerForm = (props) => {
   const [formData, setFormData] = useReducer(formReducer, {});
@@ -130,8 +49,6 @@ const CareerForm = (props) => {
         value: event.target.value
       })
     }
-
-
   }
 
 
@@ -175,9 +92,9 @@ const CareerForm = (props) => {
                         border-gray-300
                         rounded-md
                       "
-                      value={formData.title || ''}
-                      onChange={handleChange}
-                       />
+                        value={formData.title || ''}
+                        onChange={handleChange}
+                      />
                     </div>
                   </div>
 
@@ -206,8 +123,8 @@ const CareerForm = (props) => {
                         sm:text-sm
                         rounded-md
                       "
-                      value = {formData.level || 'internship'}
-                      onChange = {handleChange}
+                        value={formData.level || 'internship'}
+                        onChange={handleChange}
                       >
                         <option value="internship">Internship</option>
                         <option value="entry">Entry</option>
@@ -237,9 +154,9 @@ const CareerForm = (props) => {
                         border-gray-300
                         rounded-md
                       "
-                      value={formData.department || ''}
-                      onChange={handleChange}
-                       />
+                        value={formData.department || ''}
+                        onChange={handleChange}
+                      />
                     </div>
                   </div>
 
@@ -263,8 +180,8 @@ const CareerForm = (props) => {
                         border border-gray-300
                         rounded-md
                       "
-                      // value = {formData.summary}
-                      onchange = {handleChange}
+                        // value = {formData.summary}
+                        onchange={handleChange}
                       />
                     </div>
                   </div>
@@ -288,15 +205,15 @@ const CareerForm = (props) => {
                           px-1.5
                           text-gray-400
                         " id="headcount-minus-btn"
-                        onClick={() => {
-                          if (value > 1) {
-                            setValue(value -1)
-                            setFormData({
-                              name: "headcount",
-                              value: value -1
-                            })
-                          }
-                        }}
+                          onClick={() => {
+                            if (value > 1) {
+                              setValue(value - 1)
+                              setFormData({
+                                name: "headcount",
+                                value: value - 1
+                              })
+                            }
+                          }}
                         >
                           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path>
@@ -312,9 +229,9 @@ const CareerForm = (props) => {
                           focus:ring-pink-500 focus:border-pink-500
                           border-gray-300
                           rounded-md
-                        " 
-                        value= {value}
-                        readonly="" />
+                        "
+                          value={value}
+                          readonly="" />
                         <button type="button" class="
                           absolute
                           right-0
@@ -322,14 +239,14 @@ const CareerForm = (props) => {
                           px-1.5
                           text-gray-400
                         " id="headcount-plus-btn"
-                        name = "headcount"
-                        onClick={() => {
-                          setValue(value + 1)
-                          setFormData({
-                            name: "headcount",
-                            value: value +1
-                          })
-                        }}
+                          name="headcount"
+                          onClick={() => {
+                            setValue(value + 1)
+                            setFormData({
+                              name: "headcount",
+                              value: value + 1
+                            })
+                          }}
                         >
                           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
@@ -400,22 +317,22 @@ export const Career = () => {
 
   return (
     <main class="bg-gray-50">
-      <CareerForm addJobs={addJobs}/>
+      <CareerForm addJobs={addJobs} />
       {/* <CareerForm addJobs={addJobs}/> */}
       {/* <ShowCareerItems /> */}
       <div className="max-w-xl mx-auto p-6 space-y-5">
-      {jobsItems.map((job,index) => (
-        <CareerItem
-          index={index}
-          title={job.title}
-          department={job.department}
-          level={job.level}
-          onEdit={updateJob}
-          onDelete={removeJob}
-          key={job._id}
-        />
-      ))}
-    </div>
+        {jobsItems.map((job, index) => (
+          <CareerItem
+            index={index}
+            title={job.title}
+            department={job.department}
+            level={job.level}
+            onEdit={updateJob}
+            onDelete={removeJob}
+            key={job._id}
+          />
+        ))}
+      </div>
     </main>
   );
 };
